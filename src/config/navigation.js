5ece47a53b07683e0dbec02455608901384e11ee
Fraser Crichton <52700324+frasercrichton@users.noreply.github.com>
2023-02-08 20:01:50 +0000

    temp: content additions, rotation of meshes, image layout and more navigation.

---
+++ src/config/navigation.js
@@ -0,0 +1,34 @@
+const urls = [
+  {
+    text: 'Introduction',
+    url: '/introduction',
+    next: '/malcolm'
+  },
+
+  {
+    text: 'Malcolm',
+    url: '/malcolm',
+    next: '/context'
+  },
+  {
+    text: 'Context',
+    url: '/context',
+    next: '/pathways'
+  },
+  {
+    text: 'Pathways',
+    url: '/pathways',
+    next: '/accountability'
+  },
+  {
+    text: 'Accountability',
+    url: '/accountability',
+    next: '/about'
+  },
+  {
+    text: 'About',
+    url: '/about',
+    next: '/introduction'
+  }
+]
+export default urls
